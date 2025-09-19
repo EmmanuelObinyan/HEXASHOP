a650eed97d4438f0ab946bde9906889bbe7d124e
emmanuelobinyan <emmanuelobinyan936@gmail.com>
2025-09-19 11:43:29 +0100

    changed file location for env file and imported a depenendency for the firebase

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -5,19 +5,19 @@ import {getFirestore} from 'firebase/firestore'
 import {GoogleAuthProvider} from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+ 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:Import.meta.env.VITE_FIREBASE_APP_KEY,
+  apiKey:import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "fir-learning-464aa.firebaseapp.com",
   projectId: "fir-learning-464aa",
   storageBucket: "fir-learning-464aa.firebasestorage.app",
-  messagingSenderId:Import.meta.env.VITE_FIREBASE_MESSENGER_SENDER_ID,
-  appId:Import.meta.env.VITE_FIREBASE_APP_ID,
+  messagingSenderId:import.meta.env.VITE_FIREBASE_MESSENGER_SENDER_ID,
+  appId:import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: "G-CDXBHRH1TW"
 };
-
+console.log(import.meta.env.VITE_FIREBASE_APP_ID)
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
